cfc7d82c2df39e919502ea6868dec5c13c16a605
Volodymyr Salitrynskyi <vladimir.salitrinsky@gmail.com>
2016-10-16 23:12:07 +0300

    Add static files (.css, .jpg)

--- students/views.py
+++ students/views.py
@@ -1,9 +1,30 @@
+# coding=utf-8
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Views for students
 def students_list(request):
-    return render(request, 'students/students_list.html', {})
+    students = (
+        {'id': 1,
+         'first_name': u'Володимир',
+         'last_name': u'Салітринський',
+         'ticket': 235,
+         'image': 'img/st01.jpg'},
+        {'id': 2,
+         'first_name': u'Іван',
+         'last_name': u'Іванов',
+         'ticket': 2123,
+         'image': 'img/st02.jpg'},
+        {'id': 3,
+         'first_name': u'Петро',
+         'last_name': u'Петров',
+         'ticket': 235,
+         'image': 'img/st03.jpg'},
+    )
+    return render(request, 'students/students_list.html',
+                  {'students': students})
 
 def students_add(request):
     return HttpResponse('<h1>Student add form</h1>')
