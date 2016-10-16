3fea44e71b9f94b9c1afa7d84b6cf226d62f2d28
Volodymyr Salitrynskyi <vladimir.salitrinsky@gmail.com>
2016-10-16 21:19:32 +0300

    Modify students/views.py, studentsdb/urls.py and template

--- students/views.py
+++ students/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Views for students
+def students_list(request):
+    return render(request, 'students/students_list.html', {})
+
+def students_add(request):
+    return HttpResponse('<h1>Student add form</h1>')
+
+def students_edit(request, sid):
+    return HttpResponse('<h1>Edit student %s</h1>' % sid)
+
+def students_delete(request, sid):
+    return HttpResponse('<h1>Delete student %s</h1>' % sid)
+
+# Views for groups
+def groups_list(request):
+    return HttpResponse('<h1>Groups listing</h1>')
+
+def groups_add(request):
+    return HttpResponse('<h1>Group add form</h1>')
+
+def groups_edit(request, gid):
+    return HttpResponse('<h1>Edit group %s</h1>' % gid)
+
+def groups_delete(request, gid):
+    return HttpResponse('<h1>Delete group %s</h1>' % gid)
