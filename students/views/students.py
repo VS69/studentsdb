e0a4e51c81761e201a3735cacc4be101f44e9884
Volodymyr Salitrynskyi <vladimir.salitrinsky@gmail.com>
2016-10-20 19:36:00 +0300

    add directive include (Django templates) and add students/pagination.html

--- students/views/students.py
+++ students/views/students.py
@@ -33,28 +33,3 @@ def students_edit(request, sid):
 
 def students_delete(request, sid):
     return HttpResponse('<h1>Delete student %s</h1>' % sid)
-
-# Views for groups
-def groups_list(request):
-    groups = (
-        {'id': 1,
-         'name': u'АТ-32',
-         'captain': u'Іванов Іван'},
-        {'id': 2,
-         'name': u'АУ-34',
-         'captain': u'Петров Петро'},
-        {'id': 3,
-         'name': u'АУ-35',
-         'captain': u'Салітринський Володимир'},
-    )
-    return render(request, 'students/groups_list.html',
-                  {'groups': groups})
-
-def groups_add(request):
-    return HttpResponse('<h1>Group add form</h1>')
-
-def groups_edit(request, gid):
-    return HttpResponse('<h1>Edit group %s</h1>' % gid)
-
-def groups_delete(request, gid):
-    return HttpResponse('<h1>Delete group %s</h1>' % gid)
