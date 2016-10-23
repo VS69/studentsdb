dd43da7d5fd89523988996496ab8877ba8b29d1b
Volodymyr Salitrynskyi <vladimir.salitrinsky@gmail.com>
2016-10-24 00:11:22 +0300

    add students photos to base

--- students/views/students.py
+++ students/views/students.py
@@ -2,26 +2,12 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from ..models import Student
+
 
 # Views for students
 def students_list(request):
-    students = (
-        {'id': 1,
-         'first_name': u'Володимир',
-         'last_name': u'Салітринський',
-         'ticket': 235,
-         'image': 'img/st01.jpg'},
-        {'id': 2,
-         'first_name': u'Іван',
-         'last_name': u'Іванов',
-         'ticket': 2123,
-         'image': 'img/st02.jpg'},
-        {'id': 3,
-         'first_name': u'Петро',
-         'last_name': u'Петров',
-         'ticket': 1536,
-         'image': 'img/st03.jpg'},
-    )
+    students = Student.objects.all()
     return render(request, 'students/students_list.html',
                   {'students': students})
 
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from .settings import MEDIA_ROOT, DEBUG
+
 
 urlpatterns = patterns(
     '',
@@ -27,3 +29,12 @@ urlpatterns = patterns(
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if DEBUG:
+    # serve files from media folder
+    urlpatterns += patterns('',
+                            url(r'^media/(?P<path>.*)$',
+                                'django.views.static.serve',
+                                {'document_root': MEDIA_ROOT}
+                                )
+                            )
