9a068ac5116fe3af77a778ac47d55a2597fe6c0a
Volodymyr Salitrynskyi <vladimir.salitrinsky@gmail.com>
2016-10-30 22:05:27 +0200

    add fixture, migrate to mysql

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -60,12 +60,9 @@ WSGI_APPLICATION = 'studentsdb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
-    }
-}
+# We moved DATABASES variable to db.py module which added to .gitignore
+# so we don't keep mysql passwords in repository
+from .db import DATABASES
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
