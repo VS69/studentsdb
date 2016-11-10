1816cbf8181d67c1c182ddd764241db40d2dfe19
Volodymyr Salitrynskyi <vladimir.salitrinsky@gmail.com>
2016-11-10 20:44:34 +0200

    add form students_add

--- students/views/students.py
+++ students/views/students.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from ..models.student import Student
+from ..models.group import Group
 from ..util import paginate
 
 
@@ -26,7 +28,48 @@ def students_list(request):
 
 
 def students_add(request):
-    return HttpResponse('<h1>Student add form</h1>')
+    # was form posted?
+    if request.method == 'POST':
+        # was form add button clicked?
+        if request.POST.get('add_button') is not None:
+            # TODO: validate input from user
+            errors = {}
+
+            # if input data without errors:
+            if not errors:
+                # create student object
+                student = Student(
+                    first_name=request.POST['first_name'],
+                    last_name=request.POST['last_name'],
+                    middle_name=request.POST['middle_name'],
+                    birthday=request.POST['birthday'],
+                    photo=request.FILES['photo'],
+                    ticket=request.POST['ticket'],
+                    student_group=Group.objects.get(pk=request.POST[
+                        'student_group']),
+                    notes=request.POST['notes'],
+                )
+
+                # save it to database
+                student.save()
+
+                # redirect user to students list
+                return HttpResponseRedirect(reverse('home'))
+
+            else:
+                # render form with errors and previous user input
+                return render(request, 'students/students_add.html',
+                              {'groups': Group.objects.all().order_by('title'),
+                               'errors': errors})
+
+        # was form cancel button clicked?
+        elif request.POST.get('cancel_button') is not None:
+            # redirect to home page on cancel button
+            return HttpResponseRedirect(reverse('home'))
+    else:
+        # initial form render
+        return render(request, 'students/students_add.html',
+                      {'groups': Group.objects.all().order_by('title')})
 
 
 def students_edit(request, sid):
